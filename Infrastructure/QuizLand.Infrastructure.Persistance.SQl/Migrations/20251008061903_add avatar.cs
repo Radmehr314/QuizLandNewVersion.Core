11350f36bd6e0a95337cf3c204026bb3ef0d76f4
Radmehr314 <133379613+Radmehr314@users.noreply.github.com>
2025-10-08 10:42:16 +0330

    fix migration

---
+++ Infrastructure/QuizLand.Infrastructure.Persistance.SQl/Migrations/20251008061903_add avatar.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizLand.Infrastructure.Persistance.SQl.Migrations
+{
+    /// <inheritdoc />
+    public partial class addavatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AvatarId",
+                table: "Users",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Avatars",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avatars", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_AvatarId",
+                table: "Users",
+                column: "AvatarId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Avatars_AvatarId",
+                table: "Users",
+                column: "AvatarId",
+                principalTable: "Avatars",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Avatars_AvatarId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Avatars");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_AvatarId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "AvatarId",
+                table: "Users");
+        }
+    }
+}
